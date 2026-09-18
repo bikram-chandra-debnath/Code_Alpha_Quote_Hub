fc42cd31c9b6e5b42258af4c8ce5d264d896a47c
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-09-18 12:57:42 +0600

    Add constants

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Colors.pinkAccent;
+  static const Color lightBackground = Colors.white;
+  static const Color darkBackground = Colors.black;
+  static const Color textPrimary = Colors.pinkAccent;
+  static const Color textWhite = Colors.white;
+  static const Color textBlack = Colors.black;
+  static const Color textlight = Colors.grey;
+  static const Color error = Colors.red;
+  static const Color warning = Colors.deepOrangeAccent;
+  static const Color success = Colors.green;
+}
---
+++ lib/core/constants/texts.dart
@@ -0,0 +1,11 @@
+class AppTexts {
+  AppTexts._();
+
+  static const String quoteHub = "Quote Hub";
+  static const String quote = "Quote";
+  static const String hub = "Hub";
+
+  // button
+
+  static const String newQuote = "New Quote";
+}
